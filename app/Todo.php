6729d7c21033e75f1a6920c87f683d56b2c23dad
shiwuhao <shiwuhao@shiwuhao.com>
2017-04-19 08:41:31 +0800

    test

---
+++ app/Todo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Todo
+ *
+ * @mixin \Eloquent
+ */
+class Todo extends Model
+{
+    //
+    protected $fillble=['body'];
+}
